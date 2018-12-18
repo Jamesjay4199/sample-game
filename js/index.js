a997412e95064aaf54535509a83df7e73d811637
James John James <jamesjay4199@gmail.com>
2018-12-18 04:28:27 +0100

    fixes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 let app = new Vue({
     el: "#app",
     data: {
-        displayInstruction: false,
-        displayIntro: false,
-        isTimerRunning: false,
-        startSecond: 4,
+        index: 1,
         animals: [],
+        questions: [],
+        startSecond: 4,
         gameStarted: false,
+        hasAnswered: false,
+        displayIntro: false,
+        selectedQuestion: {},
+        isTimerRunning: false,
+        showingQuestion: false,
+        hasSelectedAnimal: false,
+        displayInstruction: false,
     },
     watch: {
     },
@@ -66,9 +72,101 @@ let app = new Vue({
         ];
 
         this.animals = this.shuffle(this.animals);
-        // while (true) {
-        //     this.mapCursor();
-        // }
+        this.questions = [
+            {
+                question: "The trapped workers in the factory______________out by the time the fire brigade came",
+                options: [
+                    "got", "had gotten", "was getting"
+                ],
+                answer: "had gotten"
+            },
+            {
+                question: "By the time the worker reached home, his children______________to bed.",
+                options: [
+                    "went", "had gone", "were going"
+                ],
+                answer: "had gone"
+            },
+            {
+                question: "I______________watched tortoise in the animal enclosure when I felt the tremors of an earth quake",
+                options: [
+                    "watched", "had watched", "was watching"
+                ],
+                answer: "was watching"
+            },
+            {
+                question: "The villagers______________when the typhoon hit the coastal areas",
+                options: [
+                    "slept", "had slept", "were sleeping"
+                ],
+                answer: "were sleeping"
+            },
+            {
+                question: "By the time the zoo keepers arrived from the den, the lion______________from the den.",
+                options: [
+                    "escaped", "had escaped", "were escaping"
+                ],
+                answer: "had escaped"
+            },
+            {
+                question: "The tsunami______________through the village leaving destruction in uts wake last year",
+                options: [
+                    "swept", "had swept", "was sweeping"
+                ],
+                answer: "swept"
+            },
+            {
+                question: "The newspaper reported that the experts______________that the phenomenon was an unnatural event",
+                options: [
+                    "claimed", "had claimed", "were claiming"
+                ],
+                answer: "had claimed"
+            },
+            {
+                question: "He______________when the door bell rang",
+                options: [
+                    "mopped", "had mopped", "was mopping"
+                ],
+                answer: "was mopping"
+            },
+            {
+                question: "The people at the beach______________themselves when the tsunami was approaching",
+                options: [
+                    "enjoyed", "had enjoyed", "were enjoying"
+                ],
+                answer: "were enjoying"
+            },
+            {
+                question: "The writer said that the inspiration for her newest novel______________from a particular movie",
+                options: [
+                    "came", "had come", "was coming"
+                ],
+                answer:"had come" 
+            },
+            {
+                question: "David's friends______________this morning once they learnt about the tsunami in his home town",
+                options: [
+                    "contacted", "had contacted", "were contacting"
+                ],
+                answer: "contacted"
+            },
+            {
+                question: "Air pollution worsened and smug______________over the sky last night",
+                options: [
+                    "hung", "had hung", "was hanging"
+                ],
+                answer: "hung"
+            },
+            {
+                question: "The trapped workers in the factory______________by the time the fire brigade came",
+                options: [
+                    "got", "had gotten", "was getting"
+                ],
+                answer: "had gotten"
+            },
+        ]
+        
+        this.questions = this.shuffle(this.questions);
     },
     methods: {
         startGame () {
@@ -90,14 +188,15 @@ let app = new Vue({
             document.querySelector('#instructionAudio').addEventListener('ended', () => {
                 this.displayInstruction = false;
                 this.startTimer();
-                document.querySelector('#app').addEventListener('mousemove', (event) => {
-                    let cursor = document.querySelector('#myCursor');
-                    cursor.style.display = "block";
-                    cursor.style.top = `${event.clientY - 65}px` ;
-                    cursor.style.left = `${event.clientX - 115}px`;
-                });
+                document.querySelector('#app').addEventListener('mousemove', this.moveListener);
             });
             this.dissipateImages();
+        },
+        moveListener (event) {
+            let cursor = document.querySelector('#myCursor');
+            cursor.style.display = "block";
+            cursor.style.top = `${event.clientY - 65}px`;
+            cursor.style.left = `${event.clientX - 115}px`;
         },
         startTimer () {
             this.isTimerRunning = true;
@@ -131,17 +230,18 @@ let app = new Vue({
         dissipateImages () {
             let row = document.createElement('div');
             row.classList = "row"
-            row.style.paddingTop="20%"
+            row.style.paddingTop="15%"
             this.animals.forEach(animal => {
                 let elem = document.createElement('div');
                 let space = document.createElement('div');
-                space.classList= "col-3";
+                space.classList= "col-2";
                 space.style.height = "50px;"
-                elem.classList = "col-2 ";
-                elem.style.height = "70px";
-                elem.style.marginLeft = "50px"
-                elem.style.marginTop = "50px";
+                elem.classList = "col-2 animals";
                 elem.style.zIndex = 2;
+                elem.style.height = "70px";
+                elem.style.cursor = "none";
+                elem.style.marginTop = "20px";
+                elem.style.marginLeft = "50px";
                 elem.style.backgroundImage = animal.image;
                 elem.addEventListener('mouseenter', () => {
                     this.changeCursor(animal.image);
@@ -158,9 +258,32 @@ let app = new Vue({
             document.querySelector('#appContainer').appendChild(row);
         },
         selectAnimal (animal, evt) {
-            evt.target.style.backgroundImage = animal.image;
-            console.log("James")
+            document.querySelector('#app').removeEventListener('mousemove', this.moveListener);
+            this.hasSelectedAnimal = true;
+            document.querySelectorAll('.animals').forEach(elem => {
+                elem.removeEventListener('mouseenter', () => {
+                    this.changeCursor(animal.image);
+                });
+                elem.removeEventListener('mouseleave', () => {
+                    this.returnCursor();
+                });
+            })
+            // evt.target.style.backgroundImage = animal.image;
+            this.selectedQuestion = this.questions[Math.floor(10 * Math.random())];
+            this.selectedQuestion.index = this.index++;
             // animal.isFound = true;
+        },
+        answer (index, isCorrect) {
+            this.hasAnswered = true;
+            setTimeout(() => {
+                document.querySelectorAll('.myCheck')[index].style.display ="block";
+            }, 100);
+            // console.log(check);
+            if (!isCorrect) {
+                setTimeout(() => {
+                    document.querySelectorAll('.myCheck')[index].style.display = "none";
+                }, 700);
+            }
         }
 
     }
